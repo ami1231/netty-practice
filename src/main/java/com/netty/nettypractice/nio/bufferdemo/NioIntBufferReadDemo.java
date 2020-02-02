6d6779364631884294796bf9ffa7af965651daea
Ami.Tsai <ami.tsai@tung-shen.com>
2020-02-02 16:59:01 +0800

    增加nio buffer change to write phase clear and compact差異性案例

--- src/main/java/com/netty/nettypractice/nio/bufferdemo/NioIntBufferReadDemo.java
+++ src/main/java/com/netty/nettypractice/nio/bufferdemo/NioIntBufferReadDemo.java
@@ -27,12 +27,66 @@ public class NioIntBufferReadDemo {
 
         System.out.println("start read value");
         while(intBuffer.position()<intBuffer.limit()){
-            System.out.println();
             int value = intBuffer.get();
             System.out.println(value);
             System.out.println("buffer position = " +intBuffer.position());
             System.out.println("buffer limit = " +intBuffer.limit());
+            System.out.println();
         }
+
+        //讀取完成後要切換回寫模式
+        System.out.println("change to write phase");
+        intBuffer.clear();
+
+        //切換回寫入模式時,可以調用clear or compact
+        //clear or compact會重置position = 0 ,limit = capacity
+        //若上次讀取未全部讀完,例如read limit = 40 position= 20,
+        // clear會將read cache直接清空
+        // compact則會將未讀的資料放入寫入狀態的array內,所以呼叫compact,position初始位置會在20
+
+//        demoClear(intBuffer);
+//        demoCompact(intBuffer);
+    }
+
+    public static void demoClear(IntBuffer intBuffer){
+        //重新裝載資源
+        fullValue(intBuffer);
+        //切換讀模式,讀取到一半打斷
+        readAndBreakValue(intBuffer);
+        intBuffer.clear();
+        System.out.println("buffer capacity = " +intBuffer.capacity());
+        System.out.println("buffer position = " +intBuffer.position());
+        System.out.println("buffer limit = " +intBuffer.limit());
+
+
+    }
+
+    public static void demoCompact(IntBuffer intBuffer){
+        //重新裝載資源
+        fullValue(intBuffer);
+        //切換讀模式,讀取到一半打斷
+        readAndBreakValue(intBuffer);
+        intBuffer.compact();
+        System.out.println("buffer capacity = " +intBuffer.capacity());
+        System.out.println("buffer position = " +intBuffer.position());
+        System.out.println("buffer limit = " +intBuffer.limit());
+    }
+
+    public static void fullValue(IntBuffer intBuffer){
+        for(int i = 1 ; i <= intBuffer.limit()-1;i++){
+            intBuffer.put(i);
+        }
+    }
+
+    public static void readAndBreakValue(IntBuffer intBuffer){
+        intBuffer.flip();
+        for(int i = 1 ; i <intBuffer.limit();i++){
+            System.out.println(intBuffer.get());
+            if(i==10){
+                break;
+            }
+        }
+
     }
 
 }
